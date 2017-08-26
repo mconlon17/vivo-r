1c460e4b384771bb82708607ff2e2c9a8b49408e
Michael Conlon <mconlon@ufl.edu>
2017-08-26 16:46:12 -0400

    Improve calling sequence, namespaces, returned object

--- sparql/vivo.query.r
+++ sparql/vivo.query.r
@@ -1,5 +1,5 @@
 vivo.query <- function(query, endpoint= 'http://localhost:8080/vivo/api/sparqlQuery',
-  email= 'vivo_root@school.edu', password= '*******', format="tsv",
+  email= 'vivo_root@mydomain.edu', password= 'v;bisons',
   ns = c(
     "rdf","<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
     "rdfs","<http://www.w3.org/2000/01/rdf-schema#>",
@@ -11,11 +11,11 @@ vivo.query <- function(query, endpoint= 'http://localhost:8080/vivo/api/sparqlQu
     "foaf","<http://xmlns.com/foaf/0.1/>",
     "obo","<http://purl.obolibrary.org/obo/>",
     "skos","<http://www.w3.org/2004/02/skos/core#>",
-    "uf","<http://vivo.school.edu/ontology/uf-extension#>",
+    "myvivo", "<http://vivo.mydomain.edu/individual/>",
     "vitrop","<http://vitro.mannlib.cornell.edu/ns/vitro/public#>",
-    "vivo","<http://vivoweb.org/ontology/core>")
+    "vivo","<http://vivoweb.org/ontology/core#>")
     ){
-  # Given an endpoint url, a query, and username and password, call
-  # A VIVO SPARQL API and return the requested triples
-  SPARQL(endpoint,q,ns=ns,extra=list(email=email,password=password),format=format)
+  # Given an endpoint url, a query, ausername and password, and namespaces, call
+  # a VIVO SPARQL API and return the requested triples
+  SPARQL(url=endpoint,query=query,extra=list(email=email,password=password), ns=ns)
 }
