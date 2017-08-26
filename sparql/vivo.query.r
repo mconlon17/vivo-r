8438603fdfeeaf1283711bccf4d52243d50360d7
Mike Conlon <mconlon@ufl.edu>
2017-08-26 16:47:15 -0400

    Update vivo.query.r

--- sparql/vivo.query.r
+++ sparql/vivo.query.r
@@ -1,5 +1,5 @@
 vivo.query <- function(query, endpoint= 'http://localhost:8080/vivo/api/sparqlQuery',
-  email= 'vivo_root@mydomain.edu', password= 'v;bisons',
+  email= 'vivo_root@mydomain.edu', password= '******',
   ns = c(
     "rdf","<http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
     "rdfs","<http://www.w3.org/2000/01/rdf-schema#>",
