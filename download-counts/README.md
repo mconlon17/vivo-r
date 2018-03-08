04a3130f544501f08023091db92b720c88d4d25d
Michael Conlon <mconlon@ufl.edu>
2018-03-08 09:19:33 -0500

    Count downloads by asset, release, and project

---
+++ download-counts/README.md
@@ -0,0 +1,6 @@
+# download-counts.py
+
+Use the GitHub API to get download counts for each asset of each release of VIVO.  Counts for assets,
+releases and grant total are produced.
+
+These counts are used in the VIVO monthly tracking data
---
+++ download-counts/download-counts.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    download-counts.py: Count downloads from VIVO repositories at GitHub.  Write  a file
+    for use in the R programs for VIVO tracking data
+
+"""
+import requests
+import json
+
+__author__ = "Michael Conlon"
+__copyright__ = "Copyright (c) 2018 Michael Conlon"
+__license__ = "Apache-2"
+__version__ = "0.0.0"
+
+releases = requests.get('https://api.github.com/repos/vivo-project/VIVO/releases')
+total_count = 0
+for release in releases.json():
+#    print json.dumps(release, indent=4)
+    release_count = 0
+    for x in release["assets"]:
+        if "download_count" in x:
+            print "\t", "Asset name", "\t", x["name"], "\t", x["download_count"]
+            release_count += x["download_count"]
+    print release["name"], "\t", "Count over assets", release_count, "\n"
+    total_count += release_count
+print "Super duper total count", total_count
