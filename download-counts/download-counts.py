2ecdc2e67c2419ce6f83abefb078bb4bc1ef701a
Michael Conlon <mconlon@ufl.edu>
2018-09-03 08:39:57 -0400

    Update to Python 3

--- download-counts/download-counts.py
+++ download-counts/download-counts.py
@@ -6,24 +6,22 @@
     for use in the R programs for VIVO tracking data
 
 """
-# TODO Use the API to get a list of repositories, iterate over them, producing counts for each
+
 import requests
-import json
 
 __author__ = "Michael Conlon"
 __copyright__ = "Copyright (c) 2018 Michael Conlon"
 __license__ = "Apache-2"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 releases = requests.get('https://api.github.com/repos/vivo-project/VIVO/releases')
 total_count = 0
 for release in releases.json():
-#    print json.dumps(release, indent=4)
     release_count = 0
     for x in release["assets"]:
         if "download_count" in x:
-            print "\t", "Asset name", "\t", x["name"], "\t", x["download_count"]
+            print("\t", "Asset name", "\t", x["name"], "\t", x["download_count"])
             release_count += x["download_count"]
-    print release["name"], "\t", "Count over assets", release_count, "\n"
+    print(release["name"], "\t", "Count over assets", release_count, "\n")
     total_count += release_count
-print "Super duper total count", total_count
+print("Super duper total count", total_count)
